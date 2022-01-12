ea4ffe6a1aed02fb4cc47c3bad304e1c559c5446
Tihomir Pavlovic <tihomir.pavlovic1112@gmail.com>
2022-01-12 13:29:39 +0100

    thank you page

---
+++ template-thankYou.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Thank You */
+get_header();
+?>
+
+<div class="thank_you_page">
+    <div class="thank_you_wrap">
+        <?php the_content(); ?>
+    </div>
+</div>
+
+<?php
+get_footer(); ?>
